7fba6061d66ea2327487e9c9cafac79d16b162a2
Tabing010102 <tabing010102@outlook.com>
2024-08-18 17:16:57 +0800

    chore: vs init

---
+++ DeepSeekTranslate/DeepSeekTranslateEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XUnity.AutoTranslator.Plugin.Core.Endpoints;
+using XUnity.AutoTranslator.Plugin.Core.Endpoints.Www;
+
+namespace DeepSeekTranslate
+{
+    public class DeepSeekTranslateEndpoint : WwwEndpoint
+    {
+        public override string Id => throw new NotImplementedException();
+
+        public override string FriendlyName => throw new NotImplementedException();
+
+        public override void Initialize(IInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCreateRequest(IWwwRequestCreationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnExtractTranslation(IWwwTranslationExtractionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
